e2b7898250cee9b471ee2de4eb5b17402b4fae07
focussash <47675402+focussash@users.noreply.github.com>
2020-02-13 09:50:22 -0800

    Updated KNN
    Fixed issues, now it functions properly;
    Added sampling, so it doesnt need to test all segments of a song
    used np.argpartition instead of my custom findKNN

--- K-NN_Classification.py
+++ K-NN_Classification.py
@@ -12,22 +12,30 @@ def kNN_Classifier(segment,Model,k,labels):
     genre = []
     Difference = np.square(Model - segment)
     Distance = np.sqrt(np.sum(Difference,axis = 1))
-    kNN = FindKNN(k,0,Distance)
+    #dist = Distance.values.tolist()
+    #kNN = FindKNN(k,0,dist)
+    kNN = np.argpartition(Distance, range(k))[:k]
     for i in kNN:
         genre.append(labels[i])
-    return genre
+    TestGenre = max(set(map(tuple, genre)), key = genre.count)
+    #print('Finished a segment')   
+    return TestGenre
 
 
-def FindKNN(k,target,array):
+def FindKNN(k,target,array):#Not currently in use
     #This function finds indices the k-nearest neighbours of target in a given array
     #To find the smallest distances, simply find k-nearest neighbours of 0
+    
     indices = []
+    values = []
     for i in range(k):
-        indices.append(100000)
-    for j in range(len(array)):
-        for i in range(k):
-            if abs(indices[i] - target) > abs(array[j]-target):
-                indices[i] = j
+        values.append(10000)
+        indices.append(0)
+    for j in range(len(array)):        
+        for p in range(k):
+            if abs(values[p] - target) > abs(array[j]-target):               
+                indices[p] = j
+                values[p] = array[j]
                 break
     return indices
 
@@ -55,13 +63,17 @@ print('Starting classification of test samples')
 FinalResults = pd.DataFrame(index = np.arange(0,NumTests),columns = ['id','category'])
 FileNumber = 0
 pausepoint = NumTests//5
-for file in paths[1:2]:
+for file in paths:
     print('Classifying file ',os.path.basename(file))
     TestGenre = ''
     content = pd.read_csv(file, header = None) #Read in the file
-    results = kNN_Classifier(content,FinalSet,3,labellist)
-    TestGenre = max(set(results), key = results.count) 
-    FinalResults.loc[FileNumber] = [os.path.basename(file)] + [TestGenre]
+    testcontent = content.sample(n=10)
+    results = testcontent.apply(kNN_Classifier, axis = 1, args = [FinalSet,10,labellist])
+    genres = results.tolist()
+    #results = kNN_Classifier(content,FinalSet,10,labellist)
+    TestGenre = max(set(map(tuple, genres)), key = genres.count)
+    GenreResult = ''.join(TestGenre)#Concatenate the resulted genre name into one word
+    FinalResults.loc[FileNumber] = [os.path.basename(file)] + [GenreResult]
     FileNumber += 1
     print('Finished',FileNumber, ' files')
-FinalResults.to_csv('TestTrial.csv', header=False,index = False,mode = 'a')
+FinalResults.to_csv('TestTrial.csv',index = False)
